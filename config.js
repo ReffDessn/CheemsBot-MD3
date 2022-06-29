7501fbd9046ebbaddf4a58edd7ed9fc6020a191a
RyanAditya <105885848+ReffDessn@users.noreply.github.com>
2022-06-29 12:57:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,10 +25,10 @@ const fs = require("fs")
 const chalk = require("chalk")
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
+global.autoReadAll = true // auto reading in all pchat gc and status (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6282252285143'] //ur owner number
+global.ownername = "RyanXd404"
+global.ytname = "YT: -" //ur yt chanel name
+global.socialm = "GitHub: RyanStore" //ur github or insta name
+global.location = "Indonesia, JawaBarat, Bandung" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "YanZBotZ" //ur bot name
+global.websitex = "https://github.com/RyanStore" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "Yannnnn Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "YannXd404"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.ownertag = ['6282252285143'] //ur owner tag
+global.ownernummenu = ['6282252285143'] //ur owner number in menu and all
+global.watermark = "YanZ BotZ Inc." //ur watermark
+global.botscript = "https://chat.whatsapp.com/BvwLZPkx6rm26ZGzNtPApi"
+global.linkz1 = "https://chat.whatsapp.com/BvwLZPkx6rm26ZGzNtPApi"
+global.linkz2 = "https://chat.whatsapp.com/B9cQxA0NWrXHtFclMS5awJ"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
